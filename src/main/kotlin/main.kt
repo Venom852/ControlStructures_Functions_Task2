ec5c86410d4854b4f173133dbc75aeef4ec825d9
Zelenkov Andrey <k1ng.stiv@yandex.ru>
2024-12-13 21:41:29 +0300

    Correction of exceeding the monthly limit

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -8,16 +8,17 @@ fun calculationCommission(card: String = "Мир", amountMonth: Int = 0, amount:
         return 0.0
     }
     return when {
-        card == "Mastercard" && amountMonth > 75_000 || amount > 75000 -> if (amount > 75_000) (amount - 75_000) * 0.006 + 20 else amount * 0.006 + 20
+        card == "Mastercard" && amountMonth > 75_000 || card == "Mastercard" && amount > 75000 ->
+            if (amount > 75_000) (amount - 75_000) * 0.006 + 20 else amount * 0.006 + 20
         card == "Visa" -> if (amount * 0.0075 > 35) amount * 0.0075 else 35.0
         else -> 0.0
     }
 }
 
 fun main() {
-    val amount = 60_001
+    val amount = 80_000
     val amountMonth = 424569 + amount
-    val card = "Mastercard"
+    val card = "Visa"
 
     val commission = (calculationCommission(card, amountMonth, amount)).roundToInt()
     println("Комиссия составляет $commission руб.")
