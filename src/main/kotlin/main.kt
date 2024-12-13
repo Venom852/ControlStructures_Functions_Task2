e6f444983a81358884015ff252c8bf9b1508ab4e
Zelenkov Andrey <k1ng.stiv@yandex.ru>
2024-12-14 01:06:46 +0300

    Correction of exceeding the monthly limit and the calculation of the commission for the mastercard

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,23 +3,24 @@ package ru.netology
 import kotlin.math.roundToInt
 
 fun calculationCommission(card: String = "Мир", amountMonth: Int = 0, amount: Int): Double {
-    if (amount > 150_000 || amountMonth >= 600_000) {
+    if (amount > 150_000 || amountMonth + amount > 600_000) {
         println("Ошибка, превышение лимита переводов")
         return 0.0
     }
     return when {
-        card == "Mastercard" && amountMonth > 75_000 || card == "Mastercard" && amount > 75000 ->
-            if (amount > 75_000) (amount - 75_000) * 0.006 + 20 else amount * 0.006 + 20
+        card == "Mastercard" && amountMonth + amount > 75_000 ->
+            if (amountMonth >= 75_000) amount * 0.006 + 20 else (amountMonth + amount - 75_000) * 0.006 + 20
         card == "Visa" -> if (amount * 0.0075 > 35) amount * 0.0075 else 35.0
         else -> 0.0
     }
 }
 
 fun main() {
-    val amount = 80_000
-    val amountMonth = 424569 + amount
-    val card = "Visa"
+    val amount = 150_000
+    var amountMonth = 75_000
+    val card = "Mastercard"
 
     val commission = (calculationCommission(card, amountMonth, amount)).roundToInt()
     println("Комиссия составляет $commission руб.")
+    amountMonth += amount
 }
